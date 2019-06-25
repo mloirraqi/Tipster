a726702de086604821e1d3d3bc07210050fdbc36
Mohamed Loirraqi <mloirraqi@fb.com>
2019-06-25 13:54:16 -0700

    Initial Commit

---
+++ Tipster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tipster
+//
+//  Created by mloirraqi on 6/25/19.
+//  Copyright © 2019 mloirraqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tipster/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tipster
+//
+//  Created by mloirraqi on 6/25/19.
+//  Copyright © 2019 mloirraqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
